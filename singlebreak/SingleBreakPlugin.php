a4a4817ac0c0d880e4a373840aa2bbf255461230
Alexander Bech <alexander@vaersaagod.no>
2016-05-20 00:36:38 +0200

    Cleaned up and added resources.

--- singlebreak/SingleBreakPlugin.php
+++ singlebreak/SingleBreakPlugin.php
@@ -1,32 +1,106 @@
 <?php
+/**
+ * SingleBreak plugin for Craft CMS
+ *
+ * SingleBreak is a twig filter that removes leading, trailing and orphaned linebreaks in your paragraphs.
+ *
+ * @author    Alexander Bech
+ * @copyright Copyright (c) 2016 Alexander Bech
+ * @link      http://vaersaagod.no
+ * @package   SingleBreak
+ * @since     1.0.0
+ */
 
 namespace Craft;
 
 class SingleBreakPlugin extends BasePlugin
 {
-	public function getName()
-	{
-		return Craft::t('Single Break');
-	}
+    /**
+     * @return mixed
+     */
+    public function init()
+    {
+    }
 
-	public function getVersion()
-	{
-		return '1.0.0';
-	}
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+         return Craft::t('SingleBreak');
+    }
 
-	public function getDeveloper()
-	{
-		return 'Alexander Bech';
-	}
+    /**
+     * @return mixed
+     */
+    public function getDescription()
+    {
+        return Craft::t('SingleBreak is a twig filter that removes leading, trailing and orphaned linebreaks in your paragraphs.');
+    }
 
-	public function getDeveloperUrl()
-	{
-		return 'http://www.vaersaagod.no';
-	}
+    /**
+     * @return string
+     */
+    public function getDocumentationUrl()
+    {
+        return 'https://github.com/alexbech/SingleBreak-Craft/blob/master/README.md';
+    }
 
-	public function addTwigExtension()
-	{
-		Craft::import('plugins.douche.twigextensions.SingleBreakTwigExtension');
-		return new SingleBreakTwigExtension();
-	}
+    /**
+     * @return string
+     */
+    public function getReleaseFeedUrl()
+    {
+        return 'https://raw.githubusercontent.com/alexbech/SingleBreak-Craft/master/releases.json';
+    }
+
+    /**
+     * @return string
+     */
+    public function getVersion()
+    {
+        return '1.0.0';
+    }
+
+    /**
+     * @return string
+     */
+    public function getSchemaVersion()
+    {
+        return '1.0.0';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloper()
+    {
+        return 'Alexander Bech';
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeveloperUrl()
+    {
+        return 'http://vaersaagod.no';
+    }
+
+    /**
+     * @return bool
+     */
+    public function hasCpSection()
+    {
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function addTwigExtension()
+    {
+        Craft::import('plugins.singlebreak.twigextensions.SingleBreakTwigExtension');
+
+        return new SingleBreakTwigExtension();
+    }
 }
--- singlebreak/twigextensions/SingleBreakTwigExtension.php
+++ singlebreak/twigextensions/SingleBreakTwigExtension.php
@@ -1,32 +1,59 @@
 <?php
+/**
+ * SingleBreak plugin for Craft CMS
+ *
+ * SingleBreak Twig Extension
+ *
+ * @author    Alexander Bech
+ * @copyright Copyright (c) 2016 Alexander Bech
+ * @link      http://vaersaagod.no
+ * @package   SingleBreak
+ * @since     1.0.0
+ */
 
 namespace Craft;
 
+use Twig_Extension;
+use Twig_Filter_Method;
+
 class SingleBreakTwigExtension extends \Twig_Extension
 {
-	protected $env;
+    /**
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'SingleBreak';
+    }
 
-	public function getName()
-	{
-		return 'Single Break';
-	}
+    /**
+     * @return array
+     */
+    public function getFilters()
+    {
+        return array(
+            'singlebreak' => new \Twig_Filter_Method($this, 'singlebreak'),
+        );
+    }
 
-	public function getFilters()
-	{
-		return array('singlebreak' => new \Twig_Filter_Method($this, 'singlebreak'));
-	}
+    /**
+    * @return array
+     */
+    public function getFunctions()
+    {
+        return array(
+            'singlebreak' => new \Twig_Function_Method($this, 'singlebreak'),
+        );
+    }
 
-	public function initRuntime(\Twig_Environment $env)
-	{
-		$this->env = $env;
-	}
-
-	public function singlebreak($value)
-	{
-    $string = str_replace("<p><br /></p>", "", $value);
-    $string = preg_replace("/<p>(<br ?\/>)*/", "<p>", $string);
-    $string = preg_replace("/(<br ?\/>)*<\/p>/", "</p>", $string);
-    return TemplateHelper::getRaw($string);
-	}
-	
+    /**
+     * @return string
+     */
+    public function singlebreak($value)
+    {
+      $string = str_replace("<p><br /></p>", "", $value);
+      $string = preg_replace("/<p>(<br ?\/>)*/", "<p>", $string);
+      $string = preg_replace("/(<br ?\/>)*<\/p>/", "</p>", $string);
+      return TemplateHelper::getRaw($string);
+    }
 }
